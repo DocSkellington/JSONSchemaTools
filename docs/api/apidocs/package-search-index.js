d4855863f7bb9bf267fc94168605dd71104f1369
Gaëtan Staquet <gaetan.staquet@gmail.com>
2022-02-10 16:43:28 +0100

    Add API documentation on website

---
+++ docs/api/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"be.ac.umons.jsonschematools"},{"l":"be.ac.umons.jsonschematools.generatorhandlers"},{"l":"be.ac.umons.jsonschematools.validatorhandlers"}]
