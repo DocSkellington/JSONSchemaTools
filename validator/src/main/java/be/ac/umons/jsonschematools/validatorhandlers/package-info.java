bce57a65996be53b4190522c592295236c2560fd
Gaëtan Staquet <gaetan.staquet@gmail.com>
2022-02-15 15:43:58 +0100

    Added package info for handlers

---
+++ validator/src/main/java/be/ac/umons/jsonschematools/validatorhandlers/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains default implementations of handlers for the validator.
+ * 
+ * These handlers work on abstracted documents. See the non-API documentation for more information.
+ */
+package be.ac.umons.jsonschematools.validatorhandlers;
